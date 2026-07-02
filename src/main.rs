6ab1b9864d7fe6b621056c2cc9db066480166a8c
teiwiet <teiwiet33@gmail.com>
2026-07-02 14:16:58 +0700

    chore: add extractor module

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,9 @@ mod harness;
 mod fuzzer;
 mod mutator;
 
+use std::process::Command;
 use harness::Harness;
 use fuzzer::Fuzzer;
 
 fn main() {
-    let harness = Harness::new(
-        "qemu-mips",
-        vec!["-L", "/home/teiwiet/firmwares/tplink/_tplink.bin.extracted/squashfs-root", "bin/busybox", "sh"],
-        2000,
-    );
-
-    let seeds = vec![
-        b"hello\n".to_vec(),
-        b"world\n".to_vec(),
-    ];
-
-    let mut fuzzer = Fuzzer::new(harness, "./crashes", seeds);
-
-    println!("[*] Fuzzing busybox cat via QEMU MIPS");
-    println!("[*] Running 10,000 iterations...\n");
-
-    fuzzer.run(10_000);
 }
